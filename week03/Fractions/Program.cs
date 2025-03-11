731d82d49d54e1f93502796932266b4a4b7a4e45
CodeFather <ncu2204@byui.edu>
2025-03-11 13:00:05 +0200

    Fractions code

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"No argument constructor returns: {fraction1.GetFractionString()} or {fraction1.GetFractionDecimal()}");
+        Console.WriteLine($"The top number is {fraction1.GetTop()} and the bottom number is {fraction1.GetBottom()}");
+
+        Fraction fraction2 = new Fraction(6);
+        Console.WriteLine($"One argument constructor returns: {fraction2.GetFractionString()} or {fraction2.GetFractionDecimal()}");
+        Console.WriteLine($"The top number is {fraction2.GetTop()} and the bottom number is {fraction2.GetBottom()}");
+
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine($"Two argument constructor returns: {fraction3.GetFractionString()} or {fraction3.GetFractionDecimal()}");
+        Console.WriteLine($"The top number is {fraction3.GetTop()} and the bottom number is {fraction3.GetBottom()}");
     }
 }
