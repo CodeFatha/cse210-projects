38e6640f4090065b0f350b72a869b66b0e67d8f4
CodeFather <ncu2204@byui.edu>
2025-03-20 23:24:24 +0200

    Adding the base and derived classes

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new("Denzel Dube", "Polynomials", "5.8", "6-14");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new("Noxolo Nkosi", "African History", "The History of Southern Africa");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
